a29d207c74c3220dd41af26f954d9b50cb168d31
Francisco Riquelme Ortiz <riquelme.ortiz@gmail.com>
2019-04-28 22:46:33 -0400

    proyecto angular

---
+++ carro-control-acceso/src/app/app.routing.ts
@@ -0,0 +1,17 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_guards';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'shoppingcart', component: ShoppingcartComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
